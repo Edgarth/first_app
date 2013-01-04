2ee341c958378d64c78549b9b14a7c1bc4d97336
Arthur <arthur.heynard@concoursmania.com>
2013-01-04 09:51:01 +0100

    youpi

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '745e7632dcb123b222f1dd30b94fe34c67a753a0dc6c2ad5b28724fdfb74a714f187e5ccbb61336aa118c161384510c2e8a9babc1568ecda34bc214636d9cbd1'
